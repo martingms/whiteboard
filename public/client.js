6665d5c7ea8579786bbb7d26ae2008ee95fe17d6
martingamm <martigam@stud.ntnu.no>
2011-03-31 17:40:18 +0200

    Initial commit

---
+++ public/client.js
@@ -0,0 +1,106 @@
+/* ----------------------------------------------------
+ *
+ *   Written by @martingamm, github.com/martingamm
+ *
+ * ----------------------------------------------------
+ */
+
+// Run once the DOM is ready
+$(document).ready(function() {
+    // Connects to socket.io on backend
+    socket.connect();
+    // Inits canvas
+    init();
+});
+
+// Initializes the canvas, is run on document.ready
+function init() {
+    // Gets the canvas element, and the size of the browser window
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    // Sets the canvas size to be the same as the browser size
+    canvas.width = width;
+    canvas.height = height;
+
+    // Binds mouse and touch events to functions
+    $(canvas).bind({
+        'mousedown':  startDraw,
+        'mousemove':  draw,
+        'mouseup':    stopDraw,
+        //FIXME These touchevents doesn't really work because touch also scrolls the page
+        'touchstart': startDraw,
+        'touchmove':  draw,
+        'touchend':   stopDraw
+    });
+ };
+
+// Triggered on mousedown, sets draw to true and updates X, Y values.
+// Since it was unclear to me, this in this context means the canvas element
+function startDraw(e) {
+    this.draw = true;
+    this.X = e.pageX;
+    this.Y = e.pageY;
+};
+
+// Triggered on mousemove, strokes a line between this.X/Y and e.pageX/Y
+function draw(e) {
+    if(this.draw) {
+        with(ctx) {
+            beginPath();
+            lineWidth = 4;
+            lineCap = 'round';
+            moveTo(this.X, this.Y);
+            lineTo(e.pageX, e.pageY);
+            stroke();
+        }
+        // Sends the data to the server via socket.io
+        socket.send({
+                        'fromX' : this.X,
+                        'fromY' : this.Y,
+                        'toX'   : e.pageX,
+                        'toY'   : e.pageY
+                    });
+        
+        this.X = e.pageX;
+        this.Y = e.pageY;
+    }
+};
+
+// Triggered on mouseup, sets draw to false
+function stopDraw() {
+    this.draw = false;
+};
+
+// Triggered when client recieves coordinate data from host
+function drawData(data) {
+    with(ctx) {
+        beginPath();
+        lineWidth = 4;
+        lineCap = 'round';
+        moveTo(data.fromX, data.fromY);
+        lineTo(data.toX, data.toY);
+        stroke();
+    }
+};
+
+
+/* Socket.IO */
+var socket = new io.Socket();
+
+// Triggered when client connects with host 
+socket.on('connect', function() { 
+    console.log('Connected with socket');
+});
+
+// Triggered when client recieves data from host
+socket.on('message', function(data) { 
+    drawData(data);
+});
+
+// Triggered when connection is lost between client and host
+socket.on('disconnect', function() {
+    console.log('Connection lost');
+});
---
+++ server.js
@@ -0,0 +1,64 @@
+var path = require('path'),
+    http = require('http'),
+    paperboy = require('paperboy'),
+    io = require('socket.io'),
+
+    PORT = 8003,
+    WEBROOT = path.join(path.dirname(__filename), 'public');
+
+// Copied from the paperboy example at: https://github.com/felixge/node-paperboy
+server = http.createServer(function(req, res) {
+    var ip = req.connection.remoteAddress;
+    paperboy
+        .deliver(WEBROOT, req, res)
+        .addHeader('Expires', 300)
+        .addHeader('X-PaperRoute', 'Node')
+        .before(function() {
+            //console.log('Received Request');
+        })
+        .after(function(statCode) {
+            log(statCode, req.url, ip);
+        })
+        .error(function(statCode, msg) {
+            res.writeHead(statCode, {'Content-Type': 'text/plain'});
+            res.end("Error " + statCode);
+            log(statCode, req.url, ip, msg);
+        })
+        .otherwise(function(err) {
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end("Error 404: File not found");
+            log(404, req.url, ip, err);
+        });
+});
+server.listen(PORT);
+
+// Logs status code, url that was requested, and the client ip to the console.
+function log(statCode, url, ip, err) {
+    var logStr = statCode + ' - ' + url + ' - ' + ip;
+    if (err) logStr += ' - ' + err;
+    console.log(logStr);
+};
+
+/* Socket.IO */
+var socket = io.listen(server);
+
+// Ghetto-db in the form of an array :p
+var db = []
+
+// Triggered when someone connects to the server
+socket.on('connection', function(client) {
+    for (i = 0; i < db.length; i++) {
+        client.send(db[i]);
+    };  
+
+    // Triggered when the connected client sends data
+    client.on('message', function(data) {
+        client.broadcast(data);
+        db.push(data);
+    });
+
+    // Triggered when someone disconnects from the server
+    client.on('disconnect', function() {
+ 
+    });
+}); 
